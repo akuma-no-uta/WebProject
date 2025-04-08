002b0244ae862cfe66a71396a6d5baf6a48d2cb8
akuma-no-uta <caotrangtinh1201@gmail.com>
2025-04-08 15:55:49 +0700

    Add files via upload

---
+++ giuaky/phpFile/tailieuAdmin.php
@@ -0,0 +1,90 @@
+<link rel="stylesheet" href="css/User.css">
+<div class="container" style ="width:1000px">
+     
+        <div class="top-bar">
+        
+
+            <div class="user" style="width:1000px">
+                <div class="user_header">
+                    <span>Danh sách tài liệu</span>
+                    <div class="user_search">
+                        <span class="material-symbols-outlined">search</span>
+                        <input type="text" placeholder="Tìm kiếm...">
+                    </div>
+                    
+                    <div class="user_cn" style="gap:20px">
+                        <a href="add_user.html">
+                        <span class="material-symbols-outlined">add</span>
+                    </a>
+                        <span class="material-symbols-outlined">remove</span>
+                        <a href="remove_user.html">
+                        <span class="material-symbols-outlined">close</span>
+                    </a>
+                    </div>
+                </div>
+            
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Tên tài liệu</th>
+                            <th>Ngày đăng</th>
+                            <th>Tác giả</th>
+                            <th>Mô tả</th>
+                            <th>Link</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Jon Snow</td>
+                            <td>2005</td>
+                            <td>(665)121-5454</td>
+                            <td>0912 Won Street, Alabama, SY 10001</td>
+                        </tr>
+
+                        <tr>
+                            <td>1</td>
+                            <td>Jon Snow</td>
+                            <td>2005</td>
+                            <td>(665)121-5454</td>
+                            <td>0912 Won Street, Alabama, SY 10001</td>
+                       
+                       
+                        </tr>
+
+                        <tr>
+                            <td>1</td>
+                            <td>Jon Snow</td>
+                            <td>2005</td>
+                            <td>(665)121-5454</td>
+                            <td>0912 Won Street, Alabama, SY 10001</td>
+                           
+                        </tr>
+
+                        <tr>
+                            <td>1</td>
+                            <td>Jon Snow</td>
+                            <td>2005</td>
+                            <td>(665)121-5454</td>
+                            <td>0912 Won Street, Alabama, SY 10001</td>
+                   
+                        </tr>
+
+                        <tr>
+                            <td>1</td>
+                            <td>Jon Snow</td>
+                            <td>2005</td>
+                            <td>(665)121-5454</td>
+                            <td>0912 Won Street, Alabama, SY 10001</td>
+                      
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
+
+        
+    </div>
