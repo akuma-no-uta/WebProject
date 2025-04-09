2d11127de1c598e07fdf742526a608f86b8873cc
huy-kun112-mm <phamhuy20057@gmail.com>
2025-04-09 11:47:17 +0700

    Add files via upload

---
+++ giuaky/htdocs/phpFile/mainpageAdmin.php
@@ -0,0 +1,123 @@
+<!--main section start-->
+<main>
+    <h1>Dash Board</h1>
+    <div class="date">
+        <input type="date">
+    </div>
+
+    <div class="insights">
+        <!--start doanh so ban hang-->
+        <div class="sales">
+            <span class="material-symbols-outlined">trending_up</span>
+            <div class="middle">
+                <div class="left">
+                    <h3>Số ngày dạy trong tuần</h3>
+                    <h1>4</h1>
+                </div>
+                <div class="progress">
+                    <svg>
+                        <circle cx="40" cy="40" r="30"></circle>
+                    </svg>
+                    <div class="number">80%</div>
+                </div>
+            </div>
+            <small>Tháng 11</small>
+        </div>
+
+        <div class="expenses">
+            <span class="material-symbols-outlined">Local_mall</span>
+            <div class="middle">
+                <div class="left">
+                    <h3>Tổng giáo trình</h3>
+                    <h1>5 bộ</h1>
+                </div>
+                <div class="progress">
+                    <svg>
+                        <circle cx="40" cy="40" r="30"></circle>
+                    </svg>
+                    <div class="number">80%</div>
+                </div>
+            </div>
+            <small>Tháng 11</small>
+        </div>
+
+
+        <div class="income">
+            <span class="material-symbols-outlined">attach_money</span>
+            <div class="middle">
+                <div class="left">
+                    <h3>Tổng thu nhập</h3>
+                    <h1>$30,000</h1>
+                </div>
+                <div class="progress">
+                    <svg>
+                        <circle cx="40" cy="40" r="30"></circle>
+                    </svg>
+                    <div class="number">80%</div>
+                </div>
+            </div>
+            <small>Tháng 11</small>
+        </div>
+        <!--end Loi nhuan-->
+
+
+    </div>
+
+    <!--Hoa don theo thu tu-->
+    <div class="recent_order">
+        <h1>Lịch đi dạy của tuần</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Tên môn</th>
+                    <th>Trường</th>
+                    <th>Mã phòng</th>
+                    <th>Hình thức</th>
+                    <th>Ngày giờ</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <tr>
+                    <td>Lập trình web</td>
+                    <td>TDTU</td>
+                    <td>1234</td>
+                    <td>Offline</td>
+                    <td>1-2-2025</td>
+
+                </tr>
+
+                <tr>
+                    <td>Lập trình mạng</td>
+                    <td>Havard</td>
+                    <td>Google Meet: abc123</td>
+                    <td>Online</td>
+                    <td>2-3-2025</td>
+                </tr>
+
+
+                <tr>
+                    <td>Karate</td>
+                    <td>FPT</td>
+                    <td>123</td>
+                    <td>Offline</td>
+                    <td>4-3-2025</td>
+                </tr>
+
+                <tr>
+                    <td>Giao thức mạng</td>
+                    <td>Bách khoa</td>
+                    <td>533</td>
+                    <td>Offline</td>
+                    <td>5-3-2025</td>
+
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+
+    <div class="show">
+        <span>Hiện danh sách chi tiết</span>
+    </div>
+</main>
